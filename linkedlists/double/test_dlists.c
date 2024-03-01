f6412622d65798975052b138cd615a7f0c50a273
Joseph Sparks <jls062015@icloud.com>
2024-03-01 09:23:15 -0500

    Dragged commits into new branch

--- linkedlists/double/test_dlists.c
+++ linkedlists/double/test_dlists.c
@@ -13,8 +13,48 @@ int main() {
         ASSERT_EQ(dn->prev, NULL);
     }
 
-    TEST("Can remove node from dlist") {
-        // Create a doubly linked list with three nodes: 1 <-> 2 <-> 3
+    TEST("Can print list") {
+        Dnode* mydlist = NULL;
+        RETURNS_STR(print_dlist, "", (mydlist));
+
+        mydlist = make_dnode(1);
+        mydlist->next = mydlist;
+        RETURNS_STR(print_dlist, "1", (mydlist));
+
+        Dnode* two = make_dnode(2);
+        two->next = mydlist;
+        mydlist->next = two;
+
+        Dnode* n = make_dnode(1024);
+        n->next = mydlist;
+        two->next = n;
+
+        RETURNS_STR(print_dlist, "1 -> 2 -> 1024", (mydlist));
+    }
+
+    TEST("Can insert Dnode in order in ascending list") {
+        // Confirm works with empty list
+        Dnode* mydlist = NULL;
+
+        Dnode* n = make_dnode(4);
+        dlist_insert_in_order(&mydlist, &n);
+        RETURNS_STR(print_dlist, "4", (mydlist));
+
+        n = make_dnode(2);
+        dlist_insert_in_order(&mydlist, &n);
+        RETURNS_STR(print_dlist, "2 -> 4", (mydlist));
+
+        n = make_dnode(3);
+        dlist_insert_in_order(&mydlist, &n);
+        RETURNS_STR(print_dlist, "2 -> 3 -> 4", (mydlist));
+
+        n = make_dnode(5);
+        dlist_insert_in_order(&mydlist, &n);
+        RETURNS_STR(print_dlist, "2 -> 3 -> 4 -> 5", (mydlist));
+    }
+
+    TEST("Can remove Dnode from dlist") {
+        // Create a doubly linked list with three Dnodes: 1 <-> 2 <-> 3
         Dnode* head = make_dnode(1);
         Dnode* node2 = make_dnode(2);
         Dnode* node3 = make_dnode(3);
